42a26602b3f9929e51c4ae00659e6144fc9100b4
fmzaar <fmzaar@gmail.com>
2024-10-24 17:02:33 -0300

    Se agregan filtros por categoria y estilos hover

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,3 +1,38 @@
+import { categoriaActiva } from "../../main";
+import { handleGetProductLocalStorage } from "../persistence/localstorage";
+import { handleRenderList } from "../views/store";
+
+const handleFilterProductsByCategory = (category) => {
+    const products = handleGetProductLocalStorage();
+
+    switch (category) {
+        case categoriaActiva:
+            handleRenderList(products);
+            break;
+        case "Hamburguesas":
+            handleRenderList(products.filter((product) => product.category === "Hamburguesas"));
+            break;
+        case "Papas":
+            handleRenderList(products.filter((product) => product.category === "Papas"));
+            break;
+        case "Gaseosas":
+            handleRenderList(products.filter((product) => product.category === "Gaseosas"));
+            break;
+        case "MayorPrecio":
+            handleRenderList(products.filter((product) => product.number > 2000));
+            break;
+        case "MenosPrecio":
+            handleRenderList(products.filter((product) => product.number < 2000));
+            break;
+        default:
+            handleRenderList(products);
+            break;
+    }
+}
+
+
+
+
 export const renderCategories = () => {
     const ulList = document.getElementById("listFilter");
     ulList.innerHTML = `
@@ -14,6 +49,7 @@ export const renderCategories = () => {
         });
     });
     const handleClick = (element) => {
+        handleFilterProductsByCategory(element.id);
         liElement.forEach((el) => {
             if (el.classList.contains("liActive")) {
                 el.classList.remove("liActive");
